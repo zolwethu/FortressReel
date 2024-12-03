bb1e988e8684e695a1636d5bebcc687ead4f8ca2
Sync-CAPACITI <zithasbu621@gmail.com>
2024-12-03 15:09:59 +0200

    movie file.json, movie banner

---
+++ src/pages/Banner.jsx
@@ -0,0 +1,15 @@
+import React, {userState, userEffect}  from "react";
+import './banner.css'
+
+function Banner() {
+    const [movies, setMovies] = useState([]);
+    const fetchData = () => {}
+
+
+    return  (
+        <div className="banner">
+        </div>
+    );
+}
+
+export default Banner;
